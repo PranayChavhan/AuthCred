574264c4a835a4d9fc519a1c8f5e44d576a9e83a
PranayChavhan <chavhanpranay48@gmail.com>
2025-03-18 20:48:43 +0530

    added / route

--- server/index.js
+++ server/index.js
@@ -14,5 +14,12 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
+// Add a root route for welcome message
+app.get("/", (req, res) => {
+  res.send("Welcome to AuthCred API! 🚀");
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT} and live url is: https://authcred.onrender.com`));
+
+// https://authcred.onrender.com
